d622afb28f0065cc2d01415137d1dc938bf5d06b
chenyuan2000 <62092293+chenyuan2000@users.noreply.github.com>
2020-03-19 17:17:48 +0800

    Add files via upload

--- ml_play.py
+++ ml_play.py
@@ -27,6 +27,8 @@ def ml_loop():
     comm.ml_ready()
 
     # 3. Start an endless loop.
+    last_x = 100
+    last_y = 400
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
@@ -53,17 +55,20 @@ def ml_loop():
             ball_y = scene_info.ball[1]
             platform_x = scene_info.platform[0]
             expect_x = 100
-            
-            '''
-            if ball_y > 100:
-                if ball_x == 0:
-                    expect_x = 
-                elif ball_x == 200:
-                    expect_x =
-            '''     
-            if (platform_x+20) < expect_x:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif (platform_x+20) > expect_x:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            else:
-                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            m = (ball_x - last_x) / (ball_y - last_y)
+            if (ball_y > 100 or ball_x == 200):
+                expect_x = (400 - ball_y) / m + ball_x
+            if expect_x > 200:
+                expect_x = expect_x - 200
+            elif expect_x < 0:
+                expect_x = 0 - expect_x
+            else:              
+                if (platform_x+20) < expect_x:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                elif (platform_x+20) > expect_x:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            last_x = ball_x
+            last_y = ball_y
+
